067e6122c1308878b3afb694b754701324249249
Altamirano27 <kenzad187@gmail.com>
2021-10-18 18:09:05 -0600

    Este es una app, cuya funcion tiene converrtir todas las medidas de convercion como km a mt, mt a km, cm a mm, etc.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Convertidor } from './src/components/Convertidor';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Convertidor/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#edff21',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/Convertidor.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+
+export const Convertidor = () => {
+    const [origen, setOrigen] = useState('cm')
+    const [destino, setDestino] = useState('mt')
+    const [convertir, setConvertir] = useState(0)
+    const [resultado, setResultado] = useState(0)
+
+    // const handleChangeOrigen = (texto: string) => {
+    //alert(texto)
+    //     setOrigen(texto)
+    //  }
+
+    const handleChangeConvertir = (texto: string) => {
+        const numero = parseFloat(texto)
+        //alert(texto)
+        setConvertir(numero)
+    }
+
+    const handleCalcular = () => {
+        if (origen === 'cm' && destino === 'mt') {
+            const result = convertir / 100
+            setResultado(result)
+        }
+
+        if (origen === 'km' && destino === 'mt') {
+            const result = convertir / 100
+            setResultado(result)
+        }
+
+        //proxima clasae hacer >>>>>> milimitros, centimetros metros y kilometros todos a en sus conjugaciones
+    }
+    return (
+
+        <View>
+            <Text>Origen</Text>
+
+            <TextInput
+                defaultValue={origen}
+                style={styles.input}
+                maxLength={2}
+                onChangeText={setOrigen}
+            />
+            <Text>Destino</Text>
+            <TextInput
+                defaultValue={destino}
+                style={styles.input}
+                onChangeText={setDestino}
+            />
+
+            <Text>Convertir</Text>
+            <TextInput
+                defaultValue={convertir.toString()}
+                style={styles.input}
+                onChangeText={
+                    (text) => handleChangeConvertir(text)
+                }
+            />
+
+            <Text>Resultado</Text>
+            <TextInput
+                defaultValue={resultado.toString()}
+                style={styles.input}
+            />
+
+            <View style={styles.button} />
+            <Button
+                title="Calcular"
+                onPress={handleCalcular} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    button: {
+        marginTop: 10,
+    },
+
+    //no neccerariamente se tiene que llamar input puede tener otro nombre.
+    input:
+    {
+        borderWidth: 3,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderColor: 'black'
+    }
+
+
+})
