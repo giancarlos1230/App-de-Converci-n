93d08163dae34fab2812b7191976d80146fcbe07
Altamirano27 <kenzad187@gmail.com>
2021-10-18 18:27:00 -0600

    Este es una app, cuya funcion tiene converrtir todas las medidas de convercion como km a mt, mt a km, cm a mm, etc.

--- src/components/Convertidor.tsx
+++ src/components/Convertidor.tsx
@@ -28,48 +28,99 @@ export const Convertidor = () => {
             const result = convertir / 100
             setResultado(result)
         }
+        if (origen === "ml" && destino === "cm") {
+            const result = convertir / 10;
+            setResultado(result)
+        }
+        if (origen === "ml" && destino === "mt") {
+            const result = convertir / 1000;
+            setResultado(result)
+        }
+        if (origen === "ml" && destino === "km") {
+            const result = convertir / 1000000;
+            setResultado(result)
+        }
+        if (origen === "cm" && destino === "ml") {
+            const result = convertir * 10;
+            setResultado(result)
+        }
+        if (origen === "cm" && destino === "mt") {
+            const result = convertir * 100;
+            setResultado(result)
+        }
+        if (origen === "cm" && destino === "km") {
+            const result = convertir * 100000;
+            setResultado(result)
+        }
+        if (origen === "mt" && destino === "ml") {
+            const result = convertir * 1000;
+            setResultado(result)
+        }
+        if (origen === "mt" && destino === "cm") {
+            const result = convertir * 100;
+            setResultado(result)
+        }
+        if (origen === "mt" && destino === "km") {
+            const result = convertir / 1000;
+            setResultado(result)
+        }
+        if (origen === "km" && destino === "ml") {
+            const result = convertir * 1000000;
+            setResultado(result)
+        }
+        if (origen === "km" && destino === "cm") {
+            const result = convertir * 100000;
+            setResultado(result)
+        }
+        if (origen === "km" && destino === "mt") {
+            const result = convertir * 1000;
+            setResultado(result)
+        }
 
-        //proxima clasae hacer >>>>>> milimitros, centimetros metros y kilometros todos a en sus conjugaciones
     }
-    return (
 
-        <View>
-            <Text>Origen</Text>
 
-            <TextInput
-                defaultValue={origen}
-                style={styles.input}
-                maxLength={2}
-                onChangeText={setOrigen}
-            />
-            <Text>Destino</Text>
-            <TextInput
-                defaultValue={destino}
-                style={styles.input}
-                onChangeText={setDestino}
-            />
+    //proxima clasae hacer >>>>>> milimitros, centimetros metros y kilometros todos a en sus conjugaciones
+}
+return (
 
-            <Text>Convertir</Text>
-            <TextInput
-                defaultValue={convertir.toString()}
-                style={styles.input}
-                onChangeText={
-                    (text) => handleChangeConvertir(text)
-                }
-            />
+    <View>
+        <Text>Origen</Text>
 
-            <Text>Resultado</Text>
-            <TextInput
-                defaultValue={resultado.toString()}
-                style={styles.input}
-            />
+        <TextInput
+            defaultValue={origen}
+            style={styles.input}
+            maxLength={2}
+            onChangeText={setOrigen}
+        />
+        <Text>Destino</Text>
+        <TextInput
+            defaultValue={destino}
+            style={styles.input}
+            onChangeText={setDestino}
+        />
 
-            <View style={styles.button} />
-            <Button
-                title="Calcular"
-                onPress={handleCalcular} />
-        </View>
-    )
+        <Text>Convertir</Text>
+        <TextInput
+            defaultValue={convertir.toString()}
+            style={styles.input}
+            onChangeText={
+                (text) => handleChangeConvertir(text)
+            }
+        />
+
+        <Text>Resultado</Text>
+        <TextInput
+            defaultValue={resultado.toString()}
+            style={styles.input}
+        />
+
+        <View style={styles.button} />
+        <Button
+            title="Calcular"
+            onPress={handleCalcular} />
+    </View>
+)
 }
 
 const styles = StyleSheet.create({
